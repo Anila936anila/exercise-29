9a63d210f2fc61dcdb4231864b52c5f76ffafe0b
Anila936anila <161087832+Anila936anila@users.noreply.github.com>
2024-02-27 13:02:59 -0800

    Favorite Fruit
    Typescript 45Q assignment exercise 29

---
+++ index.js
@@ -0,0 +1,21 @@
+"use strict";
+// Make an Arrey of your of your favourite fruit.
+// Define the Arrey of fruits.
+const favourite_Fruits = ["cherry", "mango", "pomogranate", "lychee", "grapes"];
+/// Syntex: arrayName.includes("element")
+//Check for specific fruit using independent if statements.
+if (favourite_Fruits.includes("pomogranate")) {
+    console.log("I like pomogranate!");
+}
+if (favourite_Fruits.includes("cherry")) {
+    console.log("I enjoy cherry!");
+}
+if (favourite_Fruits.includes("mango")) {
+    console.log("mangoes are yummy!");
+}
+if (favourite_Fruits.includes("lychee")) {
+    console.log("lychees are juicy!");
+}
+if (favourite_Fruits.includes("grapes")) {
+    console.log("It taste is so good!");
+}
